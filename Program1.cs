f4bd3f2534ec73bcd7e1ad632e518f0e688edabe
Maksymilian Golab <s33192@pjwstk.edu.pl>
2026-03-15 13:15:10 +0100

    Zintegrowano klase StatsHelper do klasy Program1

--- Program1.cs
+++ Program1.cs
@@ -1,9 +1,37 @@
-﻿namespace apbd_cw1_git_s33192;
+﻿using System;
+using apbd_cw1_git_s33192;
 
-public class Program1
+namespace ApbdCw1
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Console.ReadLine();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w kalkulatorze statystyk!");
+            Console.WriteLine("Wprowadź ciąg liczb oddzielonych spacją:");
+            
+            string? input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Błąd: Nie wprowadzono żadnych danych. Zamykam program.");
+                return;
+            }
+
+            string[] stringParts = input.Split(' ');
+            int[] numbers = new int[stringParts.Length];
+
+            for (int i = 0; i < stringParts.Length; i++)
+            {
+                if (int.TryParse(stringParts[i], out int number))
+                {
+                    numbers[i] = number;
+                }
+            }
+
+            Console.WriteLine("Dane zostały poprawnie wczytane i przygotowane do obliczeń.");
+            
         }
+        StatsHelper statsHelper = new StatsHelper();
+    }
 }
