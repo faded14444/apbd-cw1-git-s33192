741eab417ffc25d3022b7b92bf282c7f1b7d9382
Maksymilian Golab <s33192@pjwstk.edu.pl>
2026-03-15 13:20:09 +0100

    Dodano funkcje obliczania sredniej

--- StatsHelper.cs
+++ StatsHelper.cs
@@ -2,5 +2,24 @@
 
 public class StatsHelper
 {
+    public static double CalculateAverage(int[] values)
+    {
+        Console.WriteLine("Obliczanie średniej...");
+        if (values.Length == 0)
+        {
+            Console.WriteLine("Błąd: Nie można obliczyć średniej dla pustej tablicy.");
+            return 0;
+        }
+        return values.Sum() / values.Length;
+    }
+
+    
     
 }
+
+        
+
+        
+    
+
+
